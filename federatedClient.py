f0f96484e9af4f30622a2e329e0557e9ff60ea26
Philippe Thomas <philippe.thomas@dfki.de>
2024-02-27 11:09:37 +0100

    Explicit evaluation at the test set added

--- federatedClient.py
+++ federatedClient.py
@@ -136,3 +136,7 @@ fl.client.start_numpy_client(
     server_address="127.0.0.1:8080",
     client=FlowerClient(),
 )
+
+#Print the performance on the dev-set
+loss, acc = test(net, testloader)
+print("Accuracy on the Dev data= " + str(acc))
